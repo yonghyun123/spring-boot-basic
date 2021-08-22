57eff0fb537463403785833cbc5c8d168ca87f31
yonghyun123 <model9960@naver.com>
2021-08-22 19:36:24 +0900

    start project

---
+++ src/main/java/hello/core/member/MemberServciceImpl.java
@@ -0,0 +1,17 @@
+package hello.core.member;
+
+public class MemberServciceImpl implements MemberService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMemeber(Long id) {
+        return memberRepository.findById(id);
+    }
+    
+}
