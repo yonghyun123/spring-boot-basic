72c161c1e4fe8be522b8788971df9ca374da5a96
yonghyun123 <model9960@naver.com>
2021-08-23 22:24:19 +0900

    item order and discount policy

---
+++ src/test/java/hello/core/discount/FixDisoucntPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDisoucntPolicy implements DiscountPolicy{
+    private int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){
+            return discountFixAmount;
+        } else {
+            return = 0;
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,20 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDisoucntPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+    private final MemberRepository memberRepository= new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDisoucntPolicy();
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discount = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discount);
+    }
+
+}
